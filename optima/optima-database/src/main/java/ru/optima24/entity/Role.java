04c6f065d2a99dc890534a968dcef496e162a1c5
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-10-26 19:51:31 +0700

    hw01/ 1.2.3

--- optima/optima-database/src/main/java/ru/optima24/entity/Role.java
+++ optima/optima-database/src/main/java/ru/optima24/entity/Role.java
@@ -1,41 +1,41 @@
-//package ru.optima24.entity;
-//
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.io.Serializable;
-//import java.util.List;
-//import java.util.Objects;
-//
-//@Entity
-//@Data
-//@NoArgsConstructor
-//@Table(name = "roles")
-//public class Role implements Serializable {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private Long id;
-//
-//    @Column(name = "name", unique = true, nullable = false)
-//    private String name;
-//
-//    @ManyToMany(mappedBy = "roles")
-//    private List<User> users;
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Role role = (Role) o;
-//        return Objects.equals(id, role.id) &&
-//                Objects.equals(name, role.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, name);
-//    }
-//
-//}
+package ru.optima24.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) &&
+                Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
--- optima/optima-database/src/main/resources/db/changelog/v-1.1/02-insert-default-users.sql
+++ optima/optima-database/src/main/resources/db/changelog/v-1.1/02-insert-default-users.sql
@@ -1,16 +1,16 @@
--- INSERT INTO `request` (`number`, `date`, `nameCastomer`, `indicator`, `nameObject`, `term`, `nameExecutor`, `numberContract`, `dateContract`)
--- VALUE   (1, '2020-01-09', 'Новый город', 'ПК', 'детский садик', '2020-03-30', 'Тарновский', '1', '2020-01-09'),
---         (2, '2020-01-10', 'Арбан', 'ФФ', 'жилой дом №1', '2020-03-30', 'Федоров', '2', '2020-01-10'),
---         (3, '2020-01-10', 'Арбан', 'ФФ', 'жилой дом №2', '2020-03-30', 'Федоров', '3', '2020-01-10');
--- GO
---
---
---
--- INSERT INTO `request_users`(`request_id`, `user_id`)
--- SELECT (SELECT id FROM `request` WHERE `id` = '1'), (SELECT id FROM `users` WHERE `phone` = '89639567916')
--- UNION ALL
--- SELECT (SELECT id FROM `request` WHERE `id` = '2'), (SELECT id FROM `users` WHERE `phone` = '89999999999')
--- UNION ALL
--- SELECT (SELECT id FROM `request` WHERE `id` = '3'), (SELECT id FROM `users` WHERE `phone` = '89999999999')
--- ;
--- GO
+INSERT INTO `request` (`number`, `date`, `nameCastomer`, `indicator`, `nameObject`, `term`, `nameExecutor`, `numberContract`, `dateContract`)
+VALUE   (1, '2020-01-09', 'Новый город', 'ПК', 'детский садик', '2020-03-30', 'Тарновский', '1', '2020-01-09'),
+        (2, '2020-01-10', 'Арбан', 'ФФ', 'жилой дом №1', '2020-03-30', 'Федоров', '2', '2020-01-10'),
+        (3, '2020-01-10', 'Арбан', 'ФФ', 'жилой дом №2', '2020-03-30', 'Федоров', '3', '2020-01-10');
+GO
+
+
+
+INSERT INTO `request_users`(`request_id`, `user_id`)
+SELECT (SELECT id FROM `request` WHERE `id` = '1'), (SELECT id FROM `users` WHERE `phone` = '89639567916')
+UNION ALL
+SELECT (SELECT id FROM `request` WHERE `id` = '2'), (SELECT id FROM `users` WHERE `phone` = '89999999999')
+UNION ALL
+SELECT (SELECT id FROM `request` WHERE `id` = '3'), (SELECT id FROM `users` WHERE `phone` = '89999999999')
+;
+GO
